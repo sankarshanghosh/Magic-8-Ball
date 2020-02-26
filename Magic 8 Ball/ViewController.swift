73be3f99294848ff53b0f04d343cffa83ab4a1e1
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2020-02-26 09:09:11 +0530

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Sankarshan Ghosh on 26/02/20.
+//  Copyright © 2020 Sankarshan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
